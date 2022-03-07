0ca5d9f9920e6fa6a9cd896ce23b09a42dd61b9b
Kiril Sikov <87669848+BlitzenKriegen@users.noreply.github.com>
2022-03-06 18:59:13 -0700

    Barebones of a main file
    This is missing a bunch of stuff, but I think it can actually serve as a springboard for the rest of the project.

--- Stage2/Scheduler_logic.pl
+++ Stage2/Scheduler_logic.pl
@@ -1,1 +1,37 @@
-user_intro:- write('Enter number of timeslots.').
+% Project Leads: Kiril, Johann and Shiraz.
+% Project: Prolog Implementation of an imperative scheduler.
+% Details: TBD
+%
+%
+
+% TODO FOR ENTIRE PROGRAM:
+%		-We need to implement the data structures for rooms, classes and graphs.
+%		-We need to implement the functions to process input data.
+%		-We need to implement functions to read in files.
+%		-We need to implement output of data structures.
+%
+
+% user_intro will simply grab user input for the
+% number of timeslots that will be used for the
+% graph structure. This will likely be revised as
+% the body of a "main" function.
+%
+% TODO: User input and processing.
+user_intro:- write('Enter number of timeslots. \n').
+
+
+% read_file will read in an input file and process it
+% by error checking its information before putting it
+% inside one of the data structures used in the program
+% (class, room structure).
+%
+% TODO: Implement this function in its entirety
+% read_file(fileName, List):-
+
+% user_output will write to the console the resulting graph
+% structure to the console (ie a successful schedule). If this
+% failed to occour however, an error message will instead be
+% printed.
+%
+% TODO: Implementation of reading out the graph structure members
+user_output:- write('Schedule of the classes: \n').
