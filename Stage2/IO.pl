458602b3f5a5fc1a4c87064472ba7c1a53ba7bcb
Kiril Sikov <87669848+BlitzenKriegen@users.noreply.github.com>
2022-03-15 17:39:00 -0600

    Add files via upload

--- Stage2/IO.pl
+++ Stage2/IO.pl
@@ -1,3 +1,5 @@
+:-include('Data_Struct.pl').
+
 write_to_file(File,Text) :-
 	open(File, write, Stream),
 	write(Stream, Text), nl,
@@ -5,19 +7,33 @@ write_to_file(File,Text) :-
 	
 read_file(File) :-
 	open(File, read, Stream),
-	readRec(Stream, String),
-	close(Stream),
-	write(String).
-	
+	readLoop(Stream,String),
+	write(String),
+	close(Stream).
+
 %I think the best solution here is for some sort of
 %readline function that then places what its read into
 %a list.
+
+readLoop(Stream,String):-
+	readRec(Stream, String),
+	write(String),
+	addToTail(String,oldStruct,Struct),
+	makeEquivalent(oldStruct,Struct),
+	write(Struct).
 
 readRec(Stream, String):-
 	get_char(Stream, Ch),
 	write(Ch),
 	(
-	  Ch = end_of_file,String = [],!,write('here1\n');
-	  Ch = '\n',String = [],!,write('here2\n');
-	  readRec(Stream, Rest),write('here3\n'),String=[Ch|Rest]
+	  Ch = end_of_file,String = [],!;
+	  Ch = '\n',String = [],!;
+	  Ch = '\t',readRec(Stream,String),!;
+	  readRec(Stream, Rest),String=[Ch|Rest]
 	).
+
+addToTail(X,[],[X]).
+addToTail(X,[_|YT],[_|ZT]):- addToTail(X,YT,ZT).
+addToTail(X,[_|YT],[]):- addToTail(X,YT,[]).
+
+makeEquivalent(X,X).
