020f1548e9298673633a5b2b99c09ba86100796d
Kiril Sikov <87669848+BlitzenKriegen@users.noreply.github.com>
2022-03-16 14:48:27 -0600

    Add files via upload

--- Stage2/IO.pl
+++ Stage2/IO.pl
@@ -4,36 +4,44 @@ write_to_file(File,Text) :-
 	open(File, write, Stream),
 	write(Stream, Text), nl,
 	close(Stream).
-	
+
+%readFile processes an input file and
+%turns it into form 
+%L=[[Item1,Item2],[Item3,Item4]...].
 read_file(File) :-
 	open(File, read, Stream),
 	readLoop(Stream,String),
+	close(Stream),
 	write(String),
-	close(Stream).
+	processList(String,Struct).
 
-%I think the best solution here is for some sort of
-%readline function that then places what its read into
-%a list.
+%processList takes in a list and cordons
+%its members into elements, taking a list
+%of form L=[Item1,Item2,...,Item(n)] and
+%processing it to turn into
+%L=[[Item1,Item2],[Item3,Item4]...].
+processList(String,Struct):-
+	write(stub).
+
+%Using two SWI pre-defined functions and
+%one user defined function, readLoop() takes
+%in a file input, removes the whitespace and
+%any empty space to place it all inside one list.
+%This takes the form L=[Item1,Item2,...,Item(n)]
 
 readLoop(Stream,String):-
-	readRec(Stream, String),
-	write(String),
-	addToTail(String,oldStruct,Struct),
-	makeEquivalent(oldStruct,Struct),
-	write(Struct).
+	read_string(Stream,_,Lines),
+	split_string(Lines, "\t\n", "",Output),
+	remover("",Output,String).
 
-readRec(Stream, String):-
-	get_char(Stream, Ch),
-	write(Ch),
-	(
-	  Ch = end_of_file,String = [],!;
-	  Ch = '\n',String = [],!;
-	  Ch = '\t',readRec(Stream,String),!;
-	  readRec(Stream, Rest),String=[Ch|Rest]
-	).
+%These functors are taken from an online source
+%(https://stackoverflow.com/questions/60919189/prolog-remove-element-from-list).
+%The use of this functor in the program is to remove empty space that exist as
+%elements in the list created by readLoop.
+remover( _, [], []).
+remover( R, [R|T], T2) :- remover( R, T, T2).
+remover( R, [H|T], [H|T2]) :- H \= R, remover( R, T, T2).
 
-addToTail(X,[],[X]).
-addToTail(X,[_|YT],[_|ZT]):- addToTail(X,YT,ZT).
-addToTail(X,[_|YT],[]):- addToTail(X,YT,[]).
-
-makeEquivalent(X,X).
+%turnToElm() takes an input and puts it into
+%an array.
+turnToElm(X,[X]).
