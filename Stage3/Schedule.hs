12fbe39b6dfcec411bc653bbda29f1a5a3a75f2e
Kiril Sikov <87669848+BlitzenKriegen@users.noreply.github.com>
2022-04-04 21:13:24 -0600

    Add files via upload

--- Stage3/Schedule.hs
+++ Stage3/Schedule.hs
@@ -8,7 +8,17 @@ addEntry -- no error checking: allows duplicate and
 where
 emptySchedule :: Schedule
 emptySchedule = Sched []
+
 addEntry :: String -> Int -> Schedule -> Schedule
 addEntry str val (Sched lst) = Sched ((str,val):lst)
+
+printResults :: String -> IO()
+printResults str = putStr str
+
+toString :: Schedule -> String
+toString (Sched []) = []
+toString (Sched ((str,val):rest)) = (str ++ " " ++ show (val)) 
+                                    ++ "\n" ++ toString (Sched rest)
+
 data Schedule = Sched [(String,Int)]
     deriving (Show)
---
+++ Stage3/sched.hs
@@ -0,0 +1,1 @@
+sched :: CourseData -> Int -> IO()
