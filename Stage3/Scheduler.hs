2fc6195f57d8aa8a1007c1b7e19f90a2915816fc
joebro0919 <82411035+joebro0919@users.noreply.github.com>
2022-04-05 14:52:42 -0600

    Completed scheduler

--- Stage3/Scheduler.hs
+++ Stage3/Scheduler.hs
@@ -1,70 +1,42 @@
 module Scheduler
 (
-    --schedule,
+    schedule,
 )
 where
 import Course
 import CourseData
 import Schedule
 
-type Placement = (Int,Int)       -- a Placement is a col/row (x,y) coordinate pair
-type Solution  = [Placement]
-
-numQueens = 8
-range     = [1..numQueens]
-
-schedule :: CourseData -> Int -> [Course]
+schedule :: CourseData -> Int -> Maybe Schedule 
 claim :: [String] -> [Course] -> Int -> [Course]
 schedule' :: [Course] -> [Schedule]
+buildCourseList :: [Course] -> [Course] -> [Course]
+buildSchedule :: [Course] -> Int -> [Course]
 
-schedule [] _ = []
-schedule courseData timeSlots = let courseList = [ newCourse name ids timeSlots | (name,ids) <- courseData ]
-    in buildCourseList courseList courseList
+
+schedule courseData timeSlots 
+    | isEmptySched (head (schedule' completeCourseList)) = Nothing
+    | otherwise = Just (head (schedule' completeCourseList))
+    where completeCourseList = buildSchedule courseData timeSlots
 
 
 schedule' []  = [emptySchedule]
-schedule' (course : restOfCourses) = [ schedule  |  timeSlot <- getSlots course, let restOfCoursesClaimed = claim (getAdjacent course) restOfCourses timeSlot, let schedule = addEntry (getName course) timeSlot emptySchedule]
+schedule' (course : restOfCourses) = [ addEntry (getName course) timeSlot schedule  |  timeSlot <- getSlots course, schedule <- schedule' (claim (getAdjacent course) restOfCourses timeSlot)]
 
 claim courseNames [] timeSlot = []
 claim courseNames (course : restOfCourses) timeSlot 
     | (elem (getName course) courseNames) = (ruleOutSlot timeSlot course) : (claim courseNames restOfCourses timeSlot)
     | otherwise = copyCourse course : (claim courseNames restOfCourses timeSlot)
 
+--Builds complete course list given courseData
+buildSchedule [] _ = []
+buildSchedule courseData timeSlots =     
+    let courseList = [ newCourse name ids timeSlots | (name,ids) <- courseData ]
+    in buildCourseList courseList courseList
+
+--Adds adjacency list to a list of Courses resulting in a complete course list
 buildCourseList _ [] = []
 buildCourseList courseList (course : restOfCourses) = setAdjacent courseList course : buildCourseList courseList restOfCourses
 
-
-
-solveForColumns :: [Int] -> [Solution]
-
-solveForColumns []         = [ [] ]     -- one solution to no columns: an empty list of placements!
-
-solveForColumns (col:rest) = [ placement:solRest | solRest <- solveForColumns rest,  row <- range, let placement = (col,row), noAttack placement solRest]
-
-
-
-noAttack :: Placement -> Solution -> Bool     -- determinplacement is conflict-free with the given list of placements
-
-noAttack (x,y) = and . map noIndividualAttack
-
-     where noIndividualAttack (x2,y2) = y /= y2 && abs (y-y2) /= abs (x-x2)     -- not in same row && not in same column
-
-
-
-
-
-
-
-
-{- 
-*use with index set to 0
-- if return value is negative, course name not found in course list
-given a course name string and a list of courses, gives index for the course list element     that matches the course name
--}
-
---findCourse courseName [] index = -9999
---findCourse courseName (course : restOfCourses) index 
---   | courseName == getName course = index
---    | otherwise                    = findCourse courseName restOfCourses index+1
 
 
