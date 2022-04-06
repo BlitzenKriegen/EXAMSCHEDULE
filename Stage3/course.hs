837ba12061be01bd5d4361d247323545a099a894
joebro0919 <82411035+joebro0919@users.noreply.github.com>
2022-04-05 19:45:29 -0600

    Update course.hs

--- Stage3/course.hs
+++ Stage3/course.hs
@@ -7,6 +7,7 @@ module Course
     getAdjacent,
     getSlots,
     ruleOutSlot,
+    copyCourse
 
 )
 where
@@ -15,17 +16,23 @@ import Data.IntSet
 import CourseData
 
 newCourse :: String -> [Int] -> Int -> Course
+copyCourse :: Course -> Course
 getName :: Course -> String
-createAdjacentList :: [Course] -> Course -> [String]
-setAdjacent :: [Course] -> Course -> Course
 getAdjacent :: Course -> [String]
+getIdSet :: Course -> IntSet
+getTimeSlots :: Course -> IntSet
 getSlots :: Course -> [Int]
+setAdjacent :: [Course] -> Course -> Course
+createAdjacentList :: [Course] -> Course -> [String]
 ruleOutSlot :: Int -> Course -> Course
+
 
 data Course = Cr { course_name :: String, id_set :: IntSet, adj_courses :: [String], time_slots :: IntSet}
     deriving (Show)
 
 newCourse name list timeslots = Cr {course_name = name, id_set = fromList list,adj_courses = [ ],time_slots = fromList [1..timeslots]}
+
+copyCourse course = Cr {course_name = getName course, id_set = getIdSet course, adj_courses = getAdjacent course,time_slots = getTimeSlots course}
 
 getName Cr {course_name = course} = course
 
@@ -41,7 +48,8 @@ setAdjacent courseList course = let adjlist = createAdjacentList courseList cour
     in Cr {course_name = getName course, id_set = getIdSet course,adj_courses = adjlist,time_slots = getTimeSlots course}
 
 createAdjacentList [] course = getAdjacent course
-createAdjacentList (x:xs) course | (getName x == getName course) || (disjoint (getIdSet x) (getIdSet course)) = createAdjacentList xs course
+createAdjacentList (x:xs) course 
+    | (getName x == getName course) || (disjoint (getIdSet x) (getIdSet course)) = createAdjacentList xs course
     |otherwise = getName x:createAdjacentList xs course
     
 
