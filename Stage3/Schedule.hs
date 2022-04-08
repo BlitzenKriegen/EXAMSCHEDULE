06314bfb8cb4ceaa4800e1f15fb84f166bb71434
Kiril Sikov <87669848+BlitzenKriegen@users.noreply.github.com>
2022-04-08 12:28:03 -0600

    Add files via upload

--- Stage3/Schedule.hs
+++ Stage3/Schedule.hs
@@ -1,4 +1,5 @@
-
+{- ADT created by Marc Schroeder
+   Implementation created by Kiril and Johann -}
 module Schedule
 (
 Schedule, -- a "Schedule" ADT, with two public operations
@@ -11,14 +12,22 @@ toString, -- no error checking: allows duplicate and
 where
 emptySchedule :: Schedule
 emptySchedule = Sched []
+
+{- Appends a schedule structure by adding a new entry at the
+head of the list. -}
 addEntry :: String -> Int -> String -> Schedule -> Schedule
 addEntry str val str2 (Sched lst) = Sched ((str,val,str2):lst)
+
+{- Checks if the Schedule passes in is empty or not -}
 isEmptySched :: Schedule -> Bool
 isEmptySched (Sched lst) = lst == [] 
 
+{- Helper function -}
 printResults :: String -> IO()
 printResults str = putStr str
 
+{- Converts a schedule structure into a string formatted to go
+through putStr(). The string created has the form "Course Timeslot Room \n" -}
 toString :: Schedule -> String
 toString (Sched []) = []
 toString (Sched ((str,val,str2):rest)) = (str ++ " " ++ show (val) ++ " " ++ str2) 
